9f647aa250b9660f6549a2f7869ca813748aa758
Anthony S McIntyre <asm12@bonner01.rice.edu>
2018-07-23 14:40:31 -0500

    Changed file structure.

---
+++ combined/clean.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+rm SRAMpix0.bin
+rm SRAMpix1.bin
+rm SRAMpix2.bin
+rm SRAMhit0.bin
+rm SRAMhit1.bin
+rm SRAMhit2.bin
+rm histogram_binary.pdf
+rm histogram_source.pdf
+
---
+++ combined/move.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+FOLDER=$1
+mv SRAMhit0.bin /storage1/users/asm12/BinaryData/$FOLDER/SRAMhit0.bin  
+mv SRAMhit1.bin /storage1/users/asm12/BinaryData/$FOLDER/SRAMhit1.bin
+mv SRAMhit2.bin /storage1/users/asm12/BinaryData/$FOLDER/SRAMhit2.bin
+mv SRAMpix2.bin /storage1/users/asm12/BinaryData/$FOLDER/SRAMpix2.bin
+mv SRAMpix1.bin /storage1/users/asm12/BinaryData/$FOLDER/SRAMpix1.bin
+mv SRAMpix0.bin /storage1/users/asm12/BinaryData/$FOLDER/SRAMpix0.bin
+mv histogram_source.pdf /storage1/users/asm12/BinaryData/$FOLDER/histogram_source.pdf
+mv histogram_binary.pdf /storage1/users/asm12/BinaryData/$FOLDER/histogram_binary.pdf
